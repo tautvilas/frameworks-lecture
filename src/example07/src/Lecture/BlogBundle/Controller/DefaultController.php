2747b98760808d00fd276073168feb2d56166ad1
tautvilas <tautvilas@wix.com>
2014-10-24 18:51:24 +0300

    example07: crud

--- src/example07/src/Lecture/BlogBundle/Controller/DefaultController.php
+++ src/example07/src/Lecture/BlogBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace Lecture\BlogBundle\Controller;
 
+use Lecture\BlogBundle\Entity\Post;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
@@ -19,10 +22,29 @@ class DefaultController extends Controller
 
     /**
      * @Route("/blog", name="default_blog")
+     * @Method("GET")
      * @Template()
      */
     public function blogAction()
     {
-        return array();
+        $posts = $this->getDoctrine()->getRepository('LectureBlogBundle:Post')->findAll();
+        return array("posts" => $posts);
+    }
+
+    /**
+     * @Route("/blog", name="post_blog")
+     * @Method("POST")
+     * @Template()
+     */
+    public function postBlogAction(Request $request)
+    {
+        $post = new Post();
+        if ($request->get("post")) {
+            $post->setText($request->get("post"));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+        }
+        return $this->redirect($this->generateUrl('default_blog'));
     }
 }
